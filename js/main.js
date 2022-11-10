63fd68fc7f9442958d68eb31584daef7fb0d94a0
Timamyll <104847527+Timamyll@users.noreply.github.com>
2022-11-11 00:01:21 +0600

    Final slider

--- js/main.js
+++ js/main.js
@@ -1,3 +1,11 @@
-$(function(){
-
+$('.about__slider-slider').slick({
+    slideToShow: 1,
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 2000,
 });
